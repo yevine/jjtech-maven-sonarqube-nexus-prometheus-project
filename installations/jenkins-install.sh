97fabdb268dd109401f5ba3fad0039a9084ead2b
Anselme Numbisia <anselmenumbisia@gmail.com>
2024-07-17 11:11:04 -0600

    modified script to install jenkins for Linux ami 2023

--- installations/jenkins-install.sh
+++ installations/jenkins-install.sh
@@ -1,14 +1,30 @@
 #!/bin/bash
+# Ensure that your software packages are up to date on your instance by using the following command to perform a quick software update:
 sudo yum update –y
-sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
-sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
-sudo yum upgrade -y
-#sudo dnf install java-11-amazon-corretto -y
-sudo amazon-linux-extras install java-openjdk11 -y
+
+# Add the Jenkins repo using the following command:
+sudo wget -O /etc/yum.repos.d/jenkins.repo \
+    https://pkg.jenkins.io/redhat-stable/jenkins.repo
+
+#Import a key file from Jenkins-CI to enable installation from the package:
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
+
+sudo yum upgrade
+
+# Install Java (Amazon Linux 2023):
+sudo dnf install java-17-amazon-corretto -y
+
+# Install Jenkins
 sudo yum install jenkins -y
-sudo echo "jenkins ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
+
+# Enable the Jenkins service to start at boot:
 sudo systemctl enable jenkins
+
+# Start Jenkins as a service:
 sudo systemctl start jenkins
+
+# You can check the status of the Jenkins service using the command:
+sudo systemctl status jenkins
 
 # Installing Git
 sudo yum install git -y
